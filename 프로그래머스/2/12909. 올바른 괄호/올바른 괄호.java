e2bc486e63d02ab150a3629375fc3c5310f9c605
ibnuena <71430096+ibnuena@users.noreply.github.com>
2024-11-21 21:02:27 +0900

    [level 2] Title: 올바른 괄호, Time: 20.04 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,20 +3,15 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+        int open = 0;
+        int close = 0;
         
         Queue<Character> q = new LinkedList<>();
         
         for(int i=0; i<s.length(); i++){
             q.add(s.charAt(i));
         }
-        
-        // 큐로 앞에부터 하나씩 뺌, 닫는거 여는거 갯수 세
-        
-        int open = 0;
-        int close = 0;
-        
-        // 만약 open이 0인데 close, 또는 open < close면 글렀음
-        
+                
         for(int i=0; i<s.length(); i++){
             if(open < close){
                 return false;
@@ -33,6 +28,7 @@ class Solution {
                 open = close = 0;
             }
         }
+        
         if(open != close)
             return false;
         
